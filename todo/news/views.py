c733662c0025c27c6fc64afbe88575e97125415d
Rannie Ollit <rannie.ollit@petnet.com.ph>
2020-05-02 18:26:17 +0800

    Refactor templates

--- todo/news/views.py
+++ todo/news/views.py
@@ -4,15 +4,15 @@ from django.shortcuts import render, HttpResponse
 
 
 def news(request):
-    return render(request, 'news.html')
+    return render(request, 'pages/news.html')
 # return HttpResponse('news')
 
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'pages/index.html')
     # return HttpResponse('Home')
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'pages/contact.html')
     # return HttpResponse('Contact')
