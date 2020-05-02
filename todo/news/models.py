3cce8cb2952590cd789dbfb72cfa67ff86d6fe54
Rannie Ollit <rannie.ollit@petnet.com.ph>
2020-05-02 17:36:42 +0800

    Change model behavior

--- todo/news/models.py
+++ todo/news/models.py
@@ -4,6 +4,30 @@ from django.db import models
 
 
 class News(models.Model):
+    # By default table names created via migration makes a format like {app_name.model_name}
+    # To override this behavior, simply override it via Metaclass
+    class Meta:
+        db_table = 'news'
+        # change the pluralize behavior of model
+        verbose_name_plural = 'news'
+
     author = models.CharField(max_length=30)
     title = models.CharField(max_length=30)
     description = models.TextField()
+
+    def __str__(self):
+        return self.author
+
+
+class SportsNews(models.Model):
+    class Meta:
+        db_table = 'sports_news'
+        # change the pluralize behavior of model
+        verbose_name_plural = 'sports_news'
+
+    author = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.author
