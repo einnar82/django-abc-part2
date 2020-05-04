19c3d705756c47e09621fbad7f7a15ffc4037832
Rannie Ollit <rannie.ollit@petnet.com.ph>
2020-05-04 17:10:03 +0800

    Added form widgets

--- todo/news/forms.py
+++ todo/news/forms.py
@@ -2,7 +2,33 @@ from django import forms
 
 
 class RegistrationForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)
-    email = forms.CharField(max_length=100)
-    phone = forms.CharField(max_length=100)
+    username = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Username',
+            'required': True
+        }
+    ))
+    password = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+            'required': True,
+            'type': 'password'
+        }
+    ))
+    email = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Email Address',
+            'required': True,
+            'type': 'email'
+        }
+    ))
+    phone = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Phone',
+            'required': True
+        }
+    ))
